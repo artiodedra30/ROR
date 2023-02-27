dfa457d940f0ab1e8abd33c2b82e9d7530cb55d3
artiodedra30 <80910282+artiodedra30@users.noreply.github.com>
2023-02-27 09:44:27 +0530

    Add files via upload

---
+++ Assignment_15_02/CookLasanga.rb
@@ -0,0 +1,22 @@
+class CookLasagna 
+    EXPECTED_MINUTES_IN_OVEN = 40
+    
+    def remaining_minutes_in_oven(minutes_in_oven)
+        @remaining_minutes = EXPECTED_MINUTES_IN_OVEN - minutes_in_oven
+        puts "Remaining time : #{@remaining_minutes}"
+        
+    end
+    def total_time_in_minutes(minutes_in_oven, no_of_layers)
+        total_time_of_layers = no_of_layers * 2
+        puts "Total time for layers: #{total_time_of_layers} "
+        @total_time = total_time_of_layers + minutes_in_oven
+        puts "Total time: #{@total_time}"
+    end
+end
+
+cl = CookLasagna.new
+cl.remaining_minutes_in_oven(17)
+cl.total_time_in_minutes(17, 4)
+
+cl.remaining_minutes_in_oven(25)
+cl.total_time_in_minutes(25, 2)
