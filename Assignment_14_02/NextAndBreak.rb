859002ce9558f41594b0ffc2a29dcbe53b4bc885
artiodedra30 <80910282+artiodedra30@users.noreply.github.com>
2023-02-27 09:46:47 +0530

    Add files via upload

---
+++ Assignment_14_02/NextAndBreak.rb
@@ -0,0 +1,17 @@
+for i in 1..10
+    if i % 2 != 0 
+        next
+    else
+        print "#{i} \n"
+    end
+end
+ 
+ print "Break Example: \n"
+ 
+for j in 1..10
+    if j == 6 
+        break
+    else
+        print "#{j} \n"
+    end
+end
